849b5d86a5fdf21e586a66ad2810a153a5631b67
suzymf <suzhenyuan922@gmail.com>
2018-09-22 23:23:48 +0800

    eureka server

---
+++ spring-demo-eureka/src/main/java/org/suzy/demo/eureka/EurekaApplication.java
@@ -0,0 +1,18 @@
+package org.suzy.demo.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+
+/**
+ * @author SuZhenYuan
+ * Eureka Server startup
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaApplication.class, args);
+	}
+}
